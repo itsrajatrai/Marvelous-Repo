c4e79b1a723f08eb65584e7db90d084b66f29c76
Rajat Rai <therajatraiofficial@gmail.com>
2022-06-29 00:17:30 +0530

    Update # The collatz conjucture.py

--- # The collatz conjucture.py
+++ # The collatz conjucture.py
@@ -1,8 +1,4 @@
-# To Code Collatz Conjecture
-# By: J.T. Conklin
-# Date: 4/18/18
 
-# Recursion of Collatz Conjecture
 def collatz(number):
     if number % 2 == 0:
         return number // 2
@@ -10,28 +6,18 @@ def collatz(number):
         return 3 * number + 1
 
 # Main Program
-try:
-    li=[]
-    number = int(input("Enter a number: "))
-    while number != 1:
-        number = collatz(number)
-        li.append(number)
+li=[]
+number = int(input("Enter a number: "))
+while number != 1:
+  number = collatz(number)
+  li.append(number)
+print(li)
+import matplotlib.pyplot as plt
+import numpy
+pq=numpy.log10(li)
+plt.plot(pq)
+plt.show()
+   
+   
 
-    print(li)
-    import matplotlib.pyplot as plt
-    plt.plot(li)
-    plt.show()
-except ValueError:
-    print("Please enter an integer.")
-except KeyboardInterrupt:
-    print("\nProgram terminated by user.")
-except:
-    print("An error occurred.")
-    raise
-    exit()
-print("\nThe program completed successfully.")
-exit()
-# End of Program
-# By: J.T. Conklin
-# Date: 4/18/18
-# End of Collatz Conjecture
+
